c635bfd50c995c200c3c65b14ed59109aabbdb52
Joshar226 <josharaya226@gmail.com>
2025-06-08 21:38:41 -0600

    Native 1.0

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import server from "./server";
 
 const port = process.env.PORT || 4000
+const host = process.env.HOST || '192.168.0.13'
 
-server.listen(port, () => {
-    console.log(`REST API working in port ${port}`);
+
+server.listen(+port, host, () => {
+    console.log(`REST API working in ${host}:${port}`);
 })
